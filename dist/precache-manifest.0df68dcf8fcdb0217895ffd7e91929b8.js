9ade09e49c25088aadad94f64aad8ea02bc6cd4f
Tenotea <drtenotea@gmail.com>
2020-06-24 20:54:58 +0100

    Deploying with dist folder

---
+++ dist/precache-manifest.0df68dcf8fcdb0217895ffd7e91929b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d4e3dc85d0a4edbeb00",
+    "url": "/css/app.aa610aed.css"
+  },
+  {
+    "revision": "754d65b229c8368033d0f64bef553525",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d4e3dc85d0a4edbeb00",
+    "url": "/js/app.81aca8ba.js"
+  },
+  {
+    "revision": "62791cc6f6e2fc544656",
+    "url": "/js/chunk-vendors.62d97b71.js"
+  },
+  {
+    "revision": "101fe119bb958dcd042a0052130bf248",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
