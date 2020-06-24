0916ca9382857de4e2b00950e01ebebc1a3a85bd
Tenotea <drtenotea@gmail.com>
2020-06-24 22:03:16 +0100

    Edited vue config js for pwa

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+// Inside vue.config.js
+module.exports = {
+  // ...other vue-cli plugin options...
+  pwa: {
+    name: 'Today in History!',
+    themeColor: '#081331',
+    msTileColor: '#081331',
+    appleMobileWebAppCapable: 'no',
+    appleMobileWebAppStatusBarStyle: 'black',
+  }
+}
